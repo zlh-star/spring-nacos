f9881d3800273d9288a5008683fabd1732d6c806
lsLinHaiZhao <13865059812@139.com>
2022-08-29 17:34:27 +0800

    #spring笔记

---
+++ nacos-gateway/src/main/java/com/example/nacosgateway/NacosGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacosgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class, args);
+    }
+
+}
