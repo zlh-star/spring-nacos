249bcb930c22146bab48fc6943507dafe3a5f452
lsLinHaiZhao <13865059812@139.com>
2022-09-09 18:03:24 +0800

    #spring笔记seata

---
+++ seate-spring-jpa/order-server/src/main/java/com/example/orderserver/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.example.orderserver.controller;
+
+import com.example.common.OperationResponse;
+import com.example.common.order.OrderBo;
+import com.example.orderserver.service.OrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "下单")
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @ApiOperation(value = "下单",tags = "下单")
+    @RequestMapping("/order")
+    public OperationResponse order(@RequestBody OrderBo orderBo){
+        try {
+            return orderService.placeOrder(orderBo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return OperationResponse.builder()
+                .data(0)
+                .build();
+    }
+
+}
