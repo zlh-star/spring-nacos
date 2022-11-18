c2b214d15c377503b900afc4d686098f1c73da28
lsLinHaiZhao <13865059812@139.com>
2022-11-18 17:47:17 +0800

    #spring笔记

--- mongo-test/src/main/java/com/example/mongotest/MongoTestApplication.java
+++ mongo-test/src/main/java/com/example/mongotest/MongoTestApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MongoTestApplication {
 
     public static void main(String[] args) {
--- mongo-test/src/main/java/com/example/mongotest/config/TemplateTest.java
+++ mongo-test/src/main/java/com/example/mongotest/config/TemplateTest.java
@@ -34,6 +34,60 @@ public class TemplateTest {
     @Value("${mongodb.password}")
     private String password;
 
+    @Value("${spring.data.mongodb.option.max-connection-per-host}")
+    private int maxConnectionPerHost;
+
+    @Value("${spring.data.mongodb.option.min-connection-per-host}")
+    private int minConnectionPerHost;
+
+    @Value("${spring.data.mongodb.option.server-selection-timeout}")
+    private int serverSelectionsTimeout;
+
+    @Value("${spring.data.mongodb.option.threads-allowed-to-block-for-connection-multiplier}")
+    private int threadsAllowedToBlockForConnection;
+
+    @Value("${spring.data.mongodb.option.max-wait-time}")
+    private int maxWaitTimes;
+
+    @Value("${spring.data.mongodb.option.max-connection-idle-time}")
+    private int maxConnectionIdleTimes;
+
+    @Value("${spring.data.mongodb.option.max-connection-life-time}")
+    private int maxConnectionLifeTimes;
+
+    @Value("${spring.data.mongodb.option.connect-timeout}")
+    private int connectTimeouts;
+
+    @Value("${spring.data.mongodb.option.socket-timeout}")
+    private int socketTimeouts;
+
+    @Value("${spring.data.mongodb.option.socket-keep-alive}")
+    private boolean socketKeepAlives;
+
+    @Value("${spring.data.mongodb.option.ssl-enabled}")
+    private boolean sslEnable;
+
+    @Value("${spring.data.mongodb.option.ssl-invalid-host-name-allowed}")
+    private boolean sslInvalidHostNameAllowe;
+
+    @Value("${spring.data.mongodb.option.always-use-m-beans}")
+    private boolean alwaysUseMBean;
+
+    @Value("${spring.data.mongodb.option.heartbeat-socket-timeout}")
+    private int heartbeatSocketTimeouts;
+
+    @Value("${spring.data.mongodb.option.heartbeat-connect-timeout}")
+    private int heartbeatConnectTimeouts;
+
+    @Value("${spring.data.mongodb.option.min-heartbeat-frequency}")
+    private int minHeartbeatFrequences;
+
+    @Value("${spring.data.mongodb.option.heartbeat-frequency}")
+    private int heartbeatFrequences;
+
+    @Value("${spring.data.mongodb.option.local-threshold}")
+    private int localThresholdes;
+
   @Bean(name = "oAuthFactory")
   public MongoDbFactory factory() {
       List<MongoCredential> credentialsList=new ArrayList<>();
@@ -41,6 +95,24 @@ public class TemplateTest {
       MongoCredential mongoCredential=MongoCredential.createCredential(username,database,password.toCharArray());
      //springboot需要与spring-boot-starter-data-mongodb版本一致，否则报错
       MongoClientOptions.Builder builder = new MongoClientOptions.Builder();
+      builder.connectionsPerHost(maxConnectionPerHost)
+              .minConnectionsPerHost(minConnectionPerHost)
+              .serverSelectionTimeout(serverSelectionsTimeout)
+              .threadsAllowedToBlockForConnectionMultiplier(threadsAllowedToBlockForConnection)
+              .maxWaitTime(maxWaitTimes)
+              .maxConnectionIdleTime(maxConnectionIdleTimes)
+              .maxConnectionLifeTime(maxConnectionLifeTimes)
+              .connectTimeout(connectTimeouts)
+              .socketTimeout(socketTimeouts)
+              .socketKeepAlive(socketKeepAlives)
+              .sslEnabled(sslEnable)
+              .sslInvalidHostNameAllowed(sslInvalidHostNameAllowe)
+              .alwaysUseMBeans(alwaysUseMBean)
+              .heartbeatSocketTimeout(heartbeatSocketTimeouts)
+              .heartbeatConnectTimeout(heartbeatConnectTimeouts)
+              .minHeartbeatFrequency(minHeartbeatFrequences)
+              .heartbeatFrequency(heartbeatFrequences)
+              .localThreshold(localThresholdes);
       MongoClientOptions mongoClientOptions = builder.build();
       MongoClient mongoClient=new MongoClient(new ServerAddress(host,port),mongoCredential,mongoClientOptions);
       return new SimpleMongoDbFactory(mongoClient,dbname);
