1948931d73c45e3cd5deb24b2cea58af3e11bf77
lsLinHaiZhao <13865059812@139.com>
2022-12-15 14:49:19 +0800

    #spring笔记

--- spring-websocket/src/main/java/com/example/springwebsocket/controller/TestController.java
+++ spring-websocket/src/main/java/com/example/springwebsocket/controller/TestController.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 @RestController
 @RequestMapping("/webSocket")
@@ -21,6 +27,50 @@ public class TestController {
         }
 
     }
+
+    @RequestMapping(value = "/IP",method = RequestMethod.POST)
+    public void IP(){
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            ArrayList<String > list = new ArrayList<>();
+
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+
+                //isUp判断是否是启动状态
+                //isVirtual 判断是否是虚拟Ip
+                //isLoopback 判断是否是子网络接口
+                if (networkInterface.isUp()&&!networkInterface.isVirtual()&&!networkInterface.isLoopback()) {
+
+                    //内部网络地址
+                    Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                    while (inetAddresses.hasMoreElements()) {
+                        InetAddress inetAddress = inetAddresses.nextElement();
+                        if (inetAddress instanceof Inet4Address) {
+                            String hostAddress = inetAddress.getHostAddress();
+                            String hostName = inetAddress.getCanonicalHostName();
+                            list.add(hostAddress);
+                        }
+                    }
+                }
+            }
+            System.out.println("新方式--------"+list);
+            String IPS=String.join(",",list);
+            String[] arr = IPS.split(",");
+            String str=null;
+            int var4 = arr.length;
+            byte var5 = 4;
+            if (var5 < var4) {
+                str = arr[var5];
+            }
+            System.out.println("str:"+str);
+
+        } catch (
+                SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
 
