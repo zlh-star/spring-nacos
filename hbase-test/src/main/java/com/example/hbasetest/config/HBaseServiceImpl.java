bb3ca2ac6bfec0fc55b6ef2d5777e104d8fac9f9
lsLinHaiZhao <13865059812@139.com>
2023-07-25 11:02:51 +0800

    #spring笔记

--- hbase-test/src/main/java/com/example/hbasetest/config/HBaseServiceImpl.java
+++ hbase-test/src/main/java/com/example/hbasetest/config/HBaseServiceImpl.java
@@ -23,6 +23,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public Object createTable(String tableName,String[] cfs){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(HBaseAdmin admin = (HBaseAdmin) connection.getAdmin()){
             if(admin.tableExists(TableName.valueOf(tableName))) {
                 return "the table is existing";
@@ -50,6 +53,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public Boolean putRow(String tableName, String rowKey, String cfName,String qualifier,String data){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Put put = new Put(Bytes.toBytes(rowKey));
             put.addColumn(Bytes.toBytes(cfName), Bytes.toBytes(qualifier), Bytes.toBytes(data));
@@ -62,6 +68,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public Boolean putRows(String tableName, List<Put> puts){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             table.put(puts);
         }catch(IOException ioe){
@@ -72,6 +81,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public Result getRow(String tableName, String rowKey){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Get get = new Get(Bytes.toBytes(rowKey));
             return table.get(get);
@@ -83,6 +95,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public Result getRow(String tableName, String rowKey, FilterList filterList){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Get get = new Get(Bytes.toBytes(rowKey));
             get.setFilter(filterList);
@@ -95,6 +110,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public ResultScanner getScanner(String tableName){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Scan scan = new Scan();
             scan.setCaching(1000);
@@ -107,6 +125,9 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public ResultScanner getScanner(String tableName, String startRowKey, String endRowKey){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Scan scan = new Scan();
             scan.setStartRow(Bytes.toBytes(startRowKey));
@@ -122,6 +143,10 @@ public class HBaseServiceImpl implements HBaseService {
     // 使用过滤器
     @Override
     public ResultScanner getScanner(String tableName, String startRowKey, String endRowKey, FilterList filterList){
+
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Scan scan = new Scan();
             scan.setStartRow(Bytes.toBytes(startRowKey));
@@ -138,6 +163,9 @@ public class HBaseServiceImpl implements HBaseService {
     // 删除
     @Override
     public Boolean deleteRow(String tableName,String rowKey){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Delete delete = new Delete(Bytes.toBytes(rowKey));
             table.delete(delete);
@@ -149,7 +177,15 @@ public class HBaseServiceImpl implements HBaseService {
     // 删除 列簇 用 admin
     @Override
     public Boolean deleteColumnFamily(TableName tableName, byte[] columnFamily){
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(HBaseAdmin admin = (HBaseAdmin)connection.getAdmin()){
+            if (!admin.tableExists(tableName)||!admin.isTableDisabled(tableName)) {
+                System.out.println("表不存在或没有将表状态改为disable，导致无法删除");
+            }
+            //将表状态改为disable
+            admin.disableTable(tableName);
             admin.deleteColumnFamilyAsync(tableName,columnFamily);
         }catch(IOException ioe){
             ioe.printStackTrace();
@@ -159,6 +195,10 @@ public class HBaseServiceImpl implements HBaseService {
     //删除 某列 用 table
     @Override
     public Boolean deleteQualifier(String tableName, String rowKey, String cfName,String qualifier){
+
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         try(Table table = connection.getTable(TableName.valueOf(tableName))){
             Delete delete = new Delete(Bytes.toBytes(rowKey));
             delete.addColumn(Bytes.toBytes(cfName),Bytes.toBytes(qualifier));
@@ -172,6 +212,10 @@ public class HBaseServiceImpl implements HBaseService {
 
     @Override
     public Boolean delTableAsync(List<TableName> tableNames) throws IOException  {
+
+        if(connection.isAborted()){
+            System.out.println("服务已终止");
+        }
         HBaseAdmin admin = (HBaseAdmin)connection.getAdmin();
         if(admin!=null){
             tableNames.forEach(tableName -> {
