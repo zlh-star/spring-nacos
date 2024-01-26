e43726768ad91850b3d7a6d50f61d9669e661876
lsLinHaiZhao <13865059812@139.com>
2024-01-26 15:34:38 +0800

    #spring笔记

--- provider/src/main/java/com/example/demo/ProviderApplication.java
+++ provider/src/main/java/com/example/demo/ProviderApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DemoApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
